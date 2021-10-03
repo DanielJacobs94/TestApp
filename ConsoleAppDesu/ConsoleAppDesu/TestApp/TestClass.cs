c0ee8a033b7035b6eed3c6359173e234a396a129
Daniel Jacobs <niel.jacobs94@gmail.com>
2021-10-03 14:07:34 +0200

    added testclass

---
+++ ConsoleAppDesu/ConsoleAppDesu/TestApp/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDesu.TestApp
+{
+    public class TestClass
+    {
+
+        public string NameOfThing { get; set; }
+    }
+}
