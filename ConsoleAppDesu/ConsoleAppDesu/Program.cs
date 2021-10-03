4c7e0264fa97009c162f9f411ff21e40cc092c82
Daniel Jacobs <niel.jacobs94@gmail.com>
2021-10-03 14:02:22 +0200

    added console line

--- ConsoleAppDesu/ConsoleAppDesu/Program.cs
+++ ConsoleAppDesu/ConsoleAppDesu/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppDesu
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Anything");
         }
     }
 }
